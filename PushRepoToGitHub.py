c5efad26ea702e53169129b3057ae4e4c8858961
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-08 21:38:43 -0800

    TestThisScript

--- PushRepoToGitHub.py
+++ PushRepoToGitHub.py
@@ -1,10 +1,10 @@
 import os
 
-print("Enter Commit Message: ")
-
-message = raw_input("Enter Message: ")
+message = raw_input("Enter Commit Message: ")
 commit_command = "git commit -m " + str(message)
 
 os.system("git status")
 os.system("git add .")
 os.system(commit_command)
+
+print("Script Completed")
