5fa25c9d5fedc678603c56632190278ca997654d
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-08 21:43:52 -0800

    ThisIsTheFinalPy

--- PushRepo.py
+++ PushRepo.py
@@ -6,5 +6,6 @@ commit_command = "git commit -m " + str(message)
 os.system("git status")
 os.system("git add .")
 os.system(commit_command)
+os.system("git push -u origin master")
 
 print("Script Completed")
