574a88d62b4036f334bf222562d172e0856a1707
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-08 21:46:58 -0800

    This is a commit

--- PushRepo.py
+++ PushRepo.py
@@ -1,7 +1,7 @@
 import os
 
 message = raw_input("Enter Commit Message: ")
-commit_command = "git commit -m " + str(message)
+commit_command = "git commit -m " + "\"" + str(message) + "\""
 
 os.system("git status")
 os.system("git add .")
