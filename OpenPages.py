80cdd2d55a0587bfece727b09930a852defa61fd
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-08 21:55:46 -0800

    Added python script to open files

---
+++ OpenPages.py
@@ -0,0 +1,10 @@
+
+# Opens all the current pages in browser
+
+import os
+
+os.system("/usr/bin/open -a \"/Applications/Google Chrome.app\" \'index.html\'")
+os.system("/usr/bin/open -a \"/Applications/Google Chrome.app\" \'experience.html\'")
+os.system("/usr/bin/open -a \"/Applications/Google Chrome.app\" \'projects.html\'")
+os.system("/usr/bin/open -a \"/Applications/Google Chrome.app\" \'articles.html\'")
+os.system("/usr/bin/open -a \"/Applications/Google Chrome.app\" \'education.html\'")
